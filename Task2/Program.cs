e157bc33f4d1b54212346b913d3dd933a6d21561
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-07 14:46:31 +0300

    new folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+Console.Clear();
+
+int[] array = {1, 5, 7, 6, 9, 8, 3, 2, 2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSorts(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+
+    }
+}
+
+PrintArray(array);
+SelectionSorts(array);
+PrintArray(array);
